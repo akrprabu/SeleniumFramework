bae2e1ebc5168ea349c5d0c17d777b134fd811c2
akrprabu <akrprabu@gmail.com>
2023-08-20 21:23:28 -0400

    Docker commands is added

--- src/test/java/com/fwork/tests/BaseTest.java
+++ src/test/java/com/fwork/tests/BaseTest.java
@@ -5,6 +5,8 @@ import com.google.common.util.concurrent.Uninterruptibles;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
@@ -17,6 +19,18 @@ public class BaseTest {
     @BeforeTest
     protected void beforeTest(ITestContext context) {
         context.setAttribute("classname", this.getClass().getSimpleName()); //This is to use in data provider
+
+        //This is to start docker hub and browser containers
+        String[] command = {"cd C:\\Users\\akrpr\\Docker Workspace>", "docker-compose up -d"};
+        ProcessBuilder builder = new ProcessBuilder(command);
+        builder = builder.directory(new File("C:\\Users\\akrpr\\Docker Workspace"));
+        try {
+            Process p = builder.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Uninterruptibles.sleepUninterruptibly(30, TimeUnit.SECONDS);
     }
     @Parameters({"browser"})
     @BeforeMethod
